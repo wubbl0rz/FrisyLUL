e9421e2d929ecd5484201273eacbea42cca82acd
max <wubbl0rz@gmx.de>
2017-07-22 18:24:05 +0200

    Add project files.

---
+++ FrisyLUL/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrisyLUL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Screenshot sc = new Screenshot((Bitmap)Bitmap.FromFile(@"C:\Users\stream\Desktop\MouseWithoutBorders\ScreenCaptures\2017-07-09_30772987.JPG"));
+
+            var apiKey = "4b18865d5488957";
+
+            Console.WriteLine(sc.Width);
+            Console.WriteLine(sc.Height);
+
+            sc = sc.Extract(1600, 700, sc.Width - 1600, sc.Height - 700);
+
+            sc.Save("blub.png");
+
+            HttpClient httpClient = new HttpClient();
+
+            var content = new MultipartFormDataContent();
+
+            content.Add(new StringContent(apiKey), "apikey");
+            content.Add(new StringContent("data:image/png;base64," + sc.ToBase64(ImageFormat.Png)), "base64Image");
+
+            var result = httpClient.PostAsync("https://api.ocr.space/parse/image", content);
+
+            Console.WriteLine(result.Result.Content.ReadAsStringAsync().Result);
+
+            //httpClient.PostAsync
+
+            //var result = sc.ToBase64(ImageFormat.Jpeg);
+
+            //Console.WriteLine(result);
+
+            //base64
+        }
+    }
+}
---
+++ FrisyLUL/Screenshot.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrisyLUL
+{
+    class Screenshot : IDisposable
+    {
+        private Bitmap bitmap;
+
+        enum TernaryRasterOperations : uint
+        {
+            SRCCOPY = 0x00CC0020,
+            CAPTUREBLT = 0x40000000
+        }
+
+        [DllImport("gdi32.dll", EntryPoint = "BitBlt", SetLastError = true)]
+        static extern bool BitBlt(IntPtr hdc,
+            int nXDest,
+            int nYDest,
+            int nWidth,
+            int nHeight,
+            IntPtr hdcSrc,
+            int nXSrc,
+            int nYSrc,
+            TernaryRasterOperations dwRop);
+
+        public bool IsEmpty { get; private set; }
+        public int Height => this.IsEmpty ? 0 : this.bitmap.Height;
+        public int Width => this.IsEmpty ? 0 : this.bitmap.Width;
+
+        public Rectangle Bounds { get => new Rectangle(Point.Empty, new Size(this.Width, this.Height)); }
+
+        private Screenshot() : this(new Bitmap(1,1))
+        {
+            this.IsEmpty = true;
+        }
+
+        public Screenshot(Bitmap bitmap) {
+            this.bitmap = bitmap;
+        }
+
+        public void Save(string filename) {
+            this.bitmap.Save(filename);
+        }
+
+        public string ToBase64(ImageFormat format)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                this.bitmap.Save(memoryStream, format);
+                return Convert.ToBase64String(memoryStream.ToArray());
+            }
+        }
+
+        public Screenshot Extract(int x, int y, int width, int height) {
+            Rectangle rect = new Rectangle(x, y, width, height);
+
+            Rectangle scRect = this.Bounds;
+            scRect.Intersect(rect);
+            
+            return scRect.IsEmpty ? new Screenshot() : new Screenshot(this.bitmap.Clone(scRect, this.bitmap.PixelFormat));
+        }
+
+        /// <summary>
+        /// Takes a <see cref="Screenshot"/> of the first window with the given case insensitive <see cref="String"/> title
+        /// </summary>
+        /// <param name="windowTitle">Title of window. Cannot be null.</param>
+        /// <exception cref="ArgumentNullException">When window title is null.</exception>  
+        public static Screenshot TakeScreenshot(string windowTitle) {
+            windowTitle = windowTitle?.ToLower() ?? throw new ArgumentNullException(nameof(windowTitle));
+   
+            var handle = Process.GetProcesses()
+                .Where(proc => proc.MainWindowHandle != IntPtr.Zero && proc.MainWindowTitle.ToLower().Contains(windowTitle))
+                .Select(proc => proc.MainWindowHandle)
+                .FirstOrDefault();
+
+            if (handle == IntPtr.Zero) return new Screenshot();
+
+            Graphics source = Graphics.FromHwnd(handle);
+
+            if (source.IsVisibleClipEmpty) return new Screenshot();
+
+            Rectangle sourceWindowSize = Rectangle.Round(source.VisibleClipBounds);
+
+            Bitmap targetBitmap = new Bitmap(sourceWindowSize.Width, sourceWindowSize.Height);
+            Graphics target = Graphics.FromImage(targetBitmap);
+
+            IntPtr sourceDC = source.GetHdc();
+            IntPtr targetDC = target.GetHdc();
+
+            BitBlt(targetDC,
+                0,
+                0,
+                targetBitmap.Width,
+                targetBitmap.Height,
+                sourceDC,
+                0,
+                0,
+                TernaryRasterOperations.SRCCOPY | TernaryRasterOperations.CAPTUREBLT);
+
+            source.ReleaseHdc();
+            target.ReleaseHdc();
+            source.Dispose();
+            target.Dispose();
+
+            return new Screenshot(targetBitmap);
+        }
+
+        public void Dispose()
+        {
+            this.bitmap.Dispose();
+        }
+    }
+}
